230bceab2daf7663c762f7405c79f26e659fc9f1
JuanMa <jsanchezg62@gmail.com>
2020-07-01 17:08:52 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { LoginComponent } from './login/login.component'
+import { Npf400Component } from './shared/npf400/npf400.component'
+import { DashboardComponent } from './pages/dashboard/dashboard.component'
+import { ProgressComponent } from './pages/progress/progress.component'
+import { Graficas1Component } from './pages/graficas1/graficas1.component'
+import { HeaderComponent } from './shared/header/header.component'
+import { SidebarComponent } from './shared/sidebar/sidebar.component'
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    Npf400Component,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/npf400/npf400.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-npf400',
+  templateUrl: './npf400.component.html',
+  styles: [
+  ]
+})
+export class Npf400Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
